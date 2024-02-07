62a68f0e14ef110707502965dede059c9d18866c
Peveylun <cobzikl@gmail.com>
2024-02-07 14:11:53 +0200

    First configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,18 @@
+import express, {Express} from "express"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const HOST: string = process.env.HOST || "localhost";
+// @ts-ignore
+const PORT: number = +process.env.PORT || 80;
+
+const app: Express = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello, World");
+});
+
+app.listen(PORT, HOST, () => console.log(`Server started on ${HOST}:${PORT}`));
+
 console.log("Hello, World!")
